c5195aab63e01401f41f9353748590ea254bda87
lettenj61 <lettenj61@users.noreply.github.com>
2019-06-26 12:51:36 +0900

    decode JSON array

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -19,12 +20,15 @@ import (
 
 type options struct {
 	EscapeHTML  bool
-	Input       string
-	ListFormats bool
+	Fields      []string
 	From        string
-	To          string
-	Scan        bool
 	Indent      int
+	Input       string
+	IsArray     bool
+	ListFormats bool
+	Scan        bool
+	To          string
+	WrapInKey   string
 }
 
 var opts options
@@ -45,28 +49,8 @@ var rootCmd *cobra.Command = &cobra.Command{
 			os.Exit(0)
 		}
 
-		var absPath string
-
 		opts.From = strings.TrimSpace(strings.ToLower(opts.From))
 		opts.To = strings.TrimSpace(strings.ToLower(opts.To))
-
-		isGuess := opts.From == "" || opts.From == "guess"
-		if isGuess {
-			if opts.Input != "" && !opts.Scan {
-				absPath, _ = filepath.Abs(opts.Input)
-				ext := filepath.Ext(absPath)
-				ext = strings.TrimPrefix(ext, ".")
-				opts.From = strings.ToLower(ext)
-			} else {
-				opts.From = "json"
-			}
-		}
-
-		// initialize from options
-		if !contains(viper.SupportedExts, opts.From) {
-			log.Fatalf("error: unsupported input: %s\n", opts.From)
-		}
-		v.SetConfigType(opts.From)
 
 		// set dumper
 		switch opts.To {
@@ -83,52 +67,11 @@ var rootCmd *cobra.Command = &cobra.Command{
 			log.Fatalf("try one of: %v\n", []string{"go", "json", "yaml", "toml"})
 		}
 
-		if opts.Scan {
-			// if scan is specified, parse stdin
-			stat, _ := os.Stdin.Stat()
-			if (stat.Mode() & os.ModeCharDevice) != 0 {
-				log.Fatal("error: nothing to read from stdin\n")
-			}
-
-			stdin, _ := ioutil.ReadAll(os.Stdin)
-
-			reader := bytes.NewReader(stdin)
-			if err := v.ReadConfig(reader); err != nil {
-				log.Fatalf("error: %v\n", err)
-			}
+		if opts.From == "csv" {
+			log.Fatalln("error: not implemented!")
 		} else {
-			// else, check for input file
-			if opts.Input == "" {
-				log.Fatal("error: input is required when not reading from stdin\n")
-			}
-
-			// normalize source path if any
-			absPath, err := filepath.Abs(opts.Input)
-			if err != nil {
-				log.Fatalf("error: %v\n", err)
-			}
-
-			if stat, err := os.Stat(absPath); err != nil {
-				log.Fatalf("error: %v\n", err)
-			} else if stat.IsDir() {
-				log.Fatalf("error: please specify normal file, not directory: %s\n", absPath)
-			}
-
-			content, err := ioutil.ReadFile(absPath)
-			if err != nil {
-				log.Fatalf("error: %v\n", err)
-			}
-			v.ReadConfig(bytes.NewReader(content))
+			processConfig()
 		}
-
-		obj := v.AllSettings()
-
-		result, err := dumper(obj)
-		if err != nil {
-			log.Fatalf("error: %v\n", err)
-		}
-
-		fmt.Println(string(result))
 	},
 }
 
@@ -165,6 +108,107 @@ func prettyGoMarshaller(value interface{}) ([]byte, error) {
 	return []byte(s), nil
 }
 
+func fromArray(r io.Reader) ([]interface{}, error) {
+	result := make([]interface{}, 0)
+	d := json.NewDecoder(r)
+	err := d.Decode(&result)
+	return result, err
+}
+
+func processConfig() {
+	// configure input format
+	var absPath string
+	isGuess := opts.From == "" || opts.From == "guess"
+	if opts.IsArray {
+		opts.From = "json"
+	} else if isGuess {
+		if opts.Input != "" && !opts.Scan {
+			absPath, _ = filepath.Abs(opts.Input)
+			ext := filepath.Ext(absPath)
+			ext = strings.TrimPrefix(ext, ".")
+			opts.From = strings.ToLower(ext)
+		} else {
+			opts.From = "json"
+		}
+	}
+
+	if !contains(viper.SupportedExts, opts.From) {
+		log.Fatalf("error: unsupported input: %s\n", opts.From)
+	}
+	v.SetConfigType(opts.From)
+
+	if opts.Scan {
+		// if scan is specified, parse stdin
+		stat, _ := os.Stdin.Stat()
+		if (stat.Mode() & os.ModeCharDevice) != 0 {
+			log.Fatal("error: nothing to read from stdin\n")
+		}
+
+		stdin, _ := ioutil.ReadAll(os.Stdin)
+
+		reader := bytes.NewReader(stdin)
+		if opts.IsArray {
+			data, err := fromArray(reader)
+			if err != nil {
+				log.Fatalf("error: %v\n", err)
+			}
+			v.Set(opts.WrapInKey, data)
+		} else {
+			if err := v.ReadConfig(reader); err != nil {
+				log.Fatalf("error: %v\n", err)
+			}
+		}
+	} else {
+		// else, check for input file
+		if opts.Input == "" {
+			log.Fatalln("error: input is required when not reading from stdin")
+		}
+
+		// normalize source path if any
+		absPath, err := filepath.Abs(opts.Input)
+		if err != nil {
+			log.Fatalf("error: %v\n", err)
+		}
+
+		if stat, err := os.Stat(absPath); err != nil {
+			log.Fatalf("error: %v\n", err)
+		} else if stat.IsDir() {
+			log.Fatalf("error: please specify normal file, not directory: %s\n", absPath)
+		}
+
+		content, err := ioutil.ReadFile(absPath)
+		if err != nil {
+			log.Fatalf("error: %v\n", err)
+		}
+		reader := bytes.NewReader(content)
+
+		if opts.IsArray {
+			data, err := fromArray(reader)
+			if err != nil {
+				log.Fatalf("error: %v\n", err)
+			}
+			v.Set(opts.WrapInKey, data)
+		} else {
+			if err := v.ReadConfig(reader); err != nil {
+				log.Fatalf("error: %v\n", err)
+			}
+		}
+	}
+
+	obj := v.AllSettings()
+
+	result, err := dumper(obj)
+	if err != nil {
+		log.Fatalf("error: %v\n", err)
+	}
+
+	fmt.Println(string(result))
+}
+
+func processCSV() {
+
+}
+
 func setup() {
 	log.SetPrefix("[vmap] ")
 	pp.ColoringEnabled = false
@@ -172,13 +216,17 @@ func setup() {
 	v = viper.New()
 	opts = options{}
 
-	rootCmd.PersistentFlags().BoolVar(&opts.ListFormats, "list-formats", false, "list available input format")
-	rootCmd.PersistentFlags().BoolVarP(&opts.EscapeHTML, "escape-html", "E", false, "escape html on json output")
-	rootCmd.PersistentFlags().StringVarP(&opts.Input, "input", "i", "", "input file path")
-	rootCmd.PersistentFlags().StringVarP(&opts.From, "from", "f", "guess", "input data format")
-	rootCmd.PersistentFlags().StringVarP(&opts.To, "to", "t", "toml", "output data format")
-	rootCmd.PersistentFlags().BoolVarP(&opts.Scan, "read-stdin", "S", false, "read from stdin")
-	rootCmd.PersistentFlags().IntVarP(&opts.Indent, "indent", "n", 4, "indents for json output")
+	flagSet := rootCmd.PersistentFlags()
+
+	flagSet.BoolVar(&opts.ListFormats, "list-formats", false, "list available input format")
+	flagSet.BoolVarP(&opts.IsArray, "array", "a", false, "decode as array not map. input forced to json")
+	flagSet.BoolVarP(&opts.EscapeHTML, "escape-html", "E", false, "escape html on json output")
+	flagSet.BoolVarP(&opts.Scan, "read-stdin", "S", false, "read from stdin")
+	flagSet.StringVarP(&opts.From, "from", "f", "guess", "input data format")
+	flagSet.StringVarP(&opts.Input, "input", "i", "", "input file path")
+	flagSet.StringVarP(&opts.To, "to", "t", "toml", "output data format")
+	flagSet.StringVarP(&opts.WrapInKey, "wrap-in", "w", "data", "field name when decoding array")
+	flagSet.IntVarP(&opts.Indent, "indent", "n", 4, "indents for json output")
 }
 
 func main() {
